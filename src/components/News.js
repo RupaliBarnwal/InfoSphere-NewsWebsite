c9096551718f251c97e0a41d72ffc2a4d095d9da
1705163 <rupalibarnwal1797@gmail.com>
2023-10-02 19:19:35 +0530

    Added time, author and source

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 import NewsComponent from './NewsComponent'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+    static defaultProps={
+        country:'in',
+        pageSize:8,
+        category:'general'
+    }
+    static propsTypes={
+        country:PropTypes.string,
+        pageSize:PropTypes.number,
+        category:PropTypes.string
+    }
     
     articles= []
     constructor(){
@@ -14,52 +27,64 @@ export class News extends Component {
         }
     }
     async componentDidMount(){
-        let url="https://newsapi.org/v2/everything?domains=wsj.com&apiKey=50adc12789f742489caedf8d1546256c&page=1&pageSize=20";
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=50adc12789f742489caedf8d1546256c&page=1&pageSize=${this.props.pageSize}`;
         // let url=""
-        let data= await fetch(url);
-        let parsedData= await data.json()
-        console.log(parsedData);
-        this.setState({articles: parsedData.articles, totalResults:parsedData.totalResults})
-    }
-    handleNextClick=async ()=>{
-        console.log("Next");
-        if(this.state.page+1> Math.ceil(this.state.totalResults/20)){
-
-        }else{
-            let url=`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=50adc12789f742489caedf8d1546256c&page=${this.state.page+1}&pageSize=20`;
-        // let url=""
-        console.log(this.state.page+1)
+        this.setState({loading:true});
         let data= await fetch(url);
         let parsedData= await data.json()
         console.log(parsedData);
         this.setState({
+            articles: parsedData.articles,
+             totalResults:parsedData.totalResults,
+             loading: false
+            })
+    }
+    handleNextClick=async ()=>{
+        console.log("Next");
+        if(!(this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize))){
+
+            let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=50adc12789f742489caedf8d1546256c&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+        // let url=""
+        // console.log(this.state.page+1)
+        this.setState({loading:true});
+        let data= await fetch(url);
+        let parsedData= await data.json()
+        // console.log(parsedData);
+        this.setState({
             page:this.state.page+1,
-            articles: parsedData.articles})
+            articles: parsedData.articles,
+            loading: false
+        })
         }
         
         
     }
     handlePreClick=async()=>{
         console.log("Previous");
-        let url=`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=50adc12789f742489caedf8d1546256c&page=${this.state.page-1}&pageSize=20`;
-        console.log(this.state.page-1);
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=50adc12789f742489caedf8d1546256c&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+        // console.log(this.state.page-1);
         // let url=""
+        this.setState({loading:true})
         let data= await fetch(url);
         let parsedData= await data.json()
-        console.log(parsedData);
+        // console.log(parsedData);
         this.setState({
             page:this.state.page-1,
-            articles: parsedData.articles})
+            articles: parsedData.articles,
+            loading:false
+        })
     }
   render() {
     
     return (
       <div className="container my-3">
-        <h2>InfoSphere- Top Headlines</h2>
+        <h2 className='text-center' style={{margin:'35px 0px'}}>InfoSphere- Top Headlines</h2>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-            {this.state.articles.map((ele)=>{
+            {!this.state.loading && this.state.articles.map((ele)=>{
                 return <div className='col-md-4' key={ele.url}>
-                <NewsComponent title={ele.title ? ele.title.slice(0, 45) : ""} description={ele.description?ele.description.slice(0,88):""} imgUrl={ele.urlToImage} newsUrl={ele.url}/>
+                <NewsComponent title={ele.title ? ele.title.slice(0, 45) : ""} description={ele.description?ele.description.slice(0,88):""}
+                   imgUrl={ele.urlToImage} newsUrl={ele.url} author={ele.author} date={ele.publishedAt} source={ele.source.name}/>
             </div>
             })}
             
@@ -69,7 +94,8 @@ export class News extends Component {
             <button disabled={this.state.page<=1} type="button" className="btn btn-outline-success" onClick={this.handlePreClick}>&larr;Pre</button>
             </div>
             <div className="ms-auto p-2">
-            <button type="button" className="btn btn-outline-success" onClick={this.handleNextClick}>Next &rarr;</button>
+            <button type="button"disabled={this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-outline-success" 
+                  onClick={this.handleNextClick}>Next &rarr;</button>
             </div>
             </div>
       
--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react'
 
 export class NewsComponent extends Component {
   render() {
-    let {title, description, imgUrl, newsUrl}=this.props;
+    let {title, description, imgUrl, newsUrl, author,date,source}=this.props;
     return (
         <>
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
-  <img src={!imgUrl?"https://ichef.bbci.co.uk/news/1024/branded_news/8D21/production/_131292163_6749f9d4a61ea749f5db87c265917df912f29bb5.jpg":imgUrl} className="card-img-top" alt="..."/>
+        <div className="card">
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>
+      {source}</span>
+  <img src={!imgUrl?"https://ichef.bbci.co.uk/news/1024/branded_news/8D21/production/_131292163_6749f9d4a61ea749f5db87c265917df912f29bb5.jpg":imgUrl} 
+  className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}...</p>
+    <p className="card-text"><small className="text-muted">By {author? author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
     <a href={newsUrl} target='blank' className="btn btn-sm btn-success">Read More</a>
   </div>
 </div>
